9dc6d71c024498cfaf3ce7e7bfe6881fe4c8a978
MaksimA1 <118662976+MaksimA1@users.noreply.github.com>
2023-03-04 00:35:08 +0300

    Delete homework.js

--- src/homework.js
+++
@@ -1,9 +0,0 @@
-//Напишите функцию countDown(n), которая получает число {n} в качестве аргумента и печатает 
-//в консоль обратный отсчет, начиная с этого числа. Например, countDown(3) напечатает: 3 2 1.
-
-function countDown(n) {
-    for (let i = n; i > 0; i--) {
-      console.log(i)
-    }
-  }
-  module.exports = countDown;
