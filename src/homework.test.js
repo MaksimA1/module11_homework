32b0f6a75b965a56135d56cc197f7cdbb17ae314
MaksimA1 <118662976+MaksimA1@users.noreply.github.com>
2023-03-04 00:35:16 +0300

    Delete homework.test.js

--- src/homework.test.js
+++
@@ -1,8 +0,0 @@
-const sum = require('./homework.js');
-
-describe("test countDown", () => {
-  it("countDown from 3", () => {
-    const result = countDown(3);
-    expect(result).toBe(3,2,1);
-  });
-});
